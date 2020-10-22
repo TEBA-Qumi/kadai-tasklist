0315f47775179b5d97f053d876d731271c1514de
TEBA_Qumi <kumi938150@gmail.com>
2020-10-22 20:46:50 +0900

    Add Modify index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -32,30 +30,13 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
+     // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        //Taskのインスタンスを作成
-        Task t = new Task();
+        request.setAttribute("task", new Task());
 
-        //各フィールドの値をセット
-        String content = "退勤";
-        t.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis() ); //currentTimeMillis()で現在の秒数まで取得
-        t.setCreated_at(currentTime);
-        t.setUpdated_at(currentTime);
-
-        //生成した t オブジェクトをpersistで管理下に置く
-        em.persist(t);
-        //DBに変更内容を保存
-        em.getTransaction().commit();
-
-        //自動採番されたIDの値を表示
-        //getWriteによりPrintWriteオブジェクトとして返すことで表示する
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-        em.close();
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/new.jsp");
+        rd.forward(request, response);
     }
 
 }
